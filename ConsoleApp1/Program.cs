e76d19293ff1a4a4938a4e5c3ec163c1c43ac36e
chenlong <chenlong@LAPTOP-7708IEGA>
2023-03-14 14:13:35 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using System;
+using System.Reflection;
+static string TrimString(string str)
+{
+    if (string.IsNullOrEmpty(str))
+    {
+        return str;
+    }
+    int start = 0;
+    int end = str.Length - 1;
+    while (start < str.Length && char.IsWhiteSpace(str[start]))
+    {
+        start++;
+    }
+    while (end >= 0 && char.IsWhiteSpace(str[end]))
+    {
+        end--;
+    }
+    if (end < start)
+    {
+        return string.Empty;
+    }
+    return str.Substring(start, end - start + 1);
+}
+
+static string GetSubstringBetweenParentheses(string str)
+{
+    int start = str.IndexOf('(');
+    int end = str.IndexOf(')');
+    if (start >= 0 && end > start)
+    {
+        return str.Substring(start + 1, end - start - 1);
+    }
+    return string.Empty;
+}
+
+static function GetFuncitonInfo(string input) {
+
+    function func = new function();
+    StreamReader reader = new StreamReader(input);
+    string content = reader.ReadToEnd();
+    string str = content;
+    int leftBracketIndex = str.IndexOf("(");
+    int spaceBeforeLeftBracketIndex = str.LastIndexOf(" ", leftBracketIndex);
+    int spaceBeforesBLBI = str.LastIndexOf(" ", spaceBeforeLeftBracketIndex - 1);
+
+    string name = str.Substring(spaceBeforeLeftBracketIndex + 1, leftBracketIndex - spaceBeforeLeftBracketIndex - 1);
+    string returnValue = str.Substring(spaceBeforesBLBI + 1, spaceBeforeLeftBracketIndex - spaceBeforesBLBI - 1);
+    func.name = name;
+    func.returnValue = returnValue;
+    string paramList = GetSubstringBetweenParentheses(str);
+    string[] arr = paramList.Split(',');
+    int length = 0;
+    foreach (string s in arr)
+    {
+        string temp = TrimString(s);
+        func.param[length++] = temp.Substring(0, temp.IndexOf(" "));
+    }
+    func.length = length;
+    return func;
+}
+
+string input = "D:\\temp\\ConsoleApp1\\input.txt";
+string output = "D:\\temp\\ConsoleApp1\\output.txt";
+string test = "D:\\temp\\ConsoleApp1\\test.txt";
+
+function input_func = GetFuncitonInfo(input);
+function output_func = GetFuncitonInfo(output);
+
+Console.WriteLine(output_func.name);
+
+Dictionary<function, function> map = new Dictionary<function, function>(new functionComparer());
+map.Add(input_func, output_func);
+function test_func = GetFuncitonInfo(test);
+
+if (test_func == input_func) {
+    Console.WriteLine("456");
+}
+
+if (map.ContainsKey(test_func)) {
+    Console.WriteLine("123");
+}
---
+++ ConsoleApp1/function.cs
@@ -0,0 +1,96 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class function
+    {
+        public string name { get; set; }
+        public string returnValue { get; set; }
+        public int length { get; set; }
+
+        public string[] param = new string[20];
+
+        public static bool operator ==(function p1, function p2)
+        {
+            if (ReferenceEquals(p1, p2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
+            {
+                return false;
+            }
+
+            if(p1.name != p2.name)
+            {
+                return false;
+            }
+            if(p1.returnValue != p2.returnValue) { return false; }
+
+            if (p1.length != p2.length) return false;
+
+            for(int i = 0; i < p1.length; i++)
+            {
+                if (p1.param[i] != p2.param[i]) return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(function p1, function p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+}
+
+class functionComparer : IEqualityComparer<function>
+{
+    public bool Equals(function p1, function p2)
+    {
+        if (ReferenceEquals(p1, p2))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
+        {
+            return false;
+        }
+
+        if (p1.name != p2.name)
+        {
+            return false;
+        }
+        if (p1.returnValue != p2.returnValue) { return false; }
+
+        if (p1.length != p2.length) return false;
+
+        for (int i = 0; i < p1.length; i++)
+        {
+            if (p1.param[i] != p2.param[i]) return false;
+        }
+        return true;
+    }
+
+    public int GetHashCode( function obj)
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 23 + obj.name.GetHashCode();
+            hash = hash * 23 + obj.returnValue.GetHashCode();
+            hash = hash * 23 + obj.length.GetHashCode();
+            hash = hash * 23 + obj.param.GetHashCode();
+            return hash;
+        }
+    }
+
+    
+}
